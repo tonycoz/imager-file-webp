808221ca8c9a50aa86292d1b8e09cddcd1f26b1f
Tony Cook <tony@develop-help.com>
2018-11-26 20:02:42 +1100

    test reading multiple images at once
    updated the dump tool too

--- t/30read.t
+++ t/30read.t
@@ -16,10 +16,22 @@ my $im = Imager->new;
 {
   my @im = Imager->read_multi(file => "testimg/simpalpha.webp", type => "webp");
   is(@im, 1, "read single alpha image (using multi interface)");
+  my $check = alpha_test_image();
+  is_image_similar($im[0], $check, 2_000_000, "check for close match");
+}
+
+{
+  my @im = Imager->read_multi(file => "testimg/anim.webp", type => "webp");
+  is(@im, 2, "read 2 images with multi interface");
+  is_image_similar($im[0], test_image(), 2_000_000, "check for close match");
+  is_image_similar($im[1], alpha_test_image(), 2_000_000, "check for close match");
+}
+
+sub alpha_test_image {
   my $check = Imager->new(xsize => 20, ysize => 20, channels => 4);
   $check->box(xmax => 9, color => [ 0, 0, 255, 128], filled => 1);
   $check->box(xmin => 10, color => [ 255, 255, 0, 192 ], filled => 1);
-  is_image_similar($im[0], $check, 2_000_000, "check for close match");
+  $check;
 }
 
 done_testing();
--- tools/dump-webp.pl
+++ tools/dump-webp.pl
@@ -21,32 +21,37 @@ Header:
   WEBP: $webp
 EOS
 
-my $chead;
-while (read($fh, $chead, 8) == 8) {
-  my ($cfourcc, $csize) = unpack("a4L<", $chead);
-  my $cbody = "";
-  if ($csize) {
-    read($fh, $cbody, $csize) == $csize
-      or die "Cannot read chunk body for $cfourcc\n";
-  }
-  (my $disp4cc = $cfourcc) =~ s/([^!-~])/ sprintf("\\x%02x", ord($1)) /eg;
-  print "Chunk: '$disp4cc' ($csize bytes)\n";
-  if ($cfourcc eq 'VP8 ') {
-    if ($csize >= 10) {
-      my ($ft, $mag, $width, $height) = unpack("a3a3S<S<", $cbody);
-      $ft = "$ft\0";
-      $ft = unpack("L<", $ft);
-      my $frame_type = ($ft >> 23) & 1 ? "inter" : "key";
-      my $version = ($ft >> 20) & 7;
-      my $show_frame = ($ft >> 19) & 1;
-      $ft &= 0x7FFFF;
-      my $mag_status = ($mag eq "\x9d\x01\x2a") ? "Good" : "Bad";
-      my $disp_mag = unpack "H*", $mag;
-      my $hscale = ($width >> 14) & 3;
-      my $vscale = ($height >> 14) & 3;
-      $width &= 0x3fff;
-      $height &= 0x3fff;
-      print <<EOS;
+do_dump($fh, 0);
+
+sub do_dump {
+  my ($fh, $indent) = @_;
+  
+  my $chead;
+  while (read($fh, $chead, 8) == 8) {
+    my ($cfourcc, $csize) = unpack("a4L<", $chead);
+    my $cbody = "";
+    if ($csize) {
+      read($fh, $cbody, $csize) == $csize
+	or die "Cannot read chunk body for $cfourcc\n";
+    }
+    (my $disp4cc = $cfourcc) =~ s/([^!-~])/ sprintf("\\x%02x", ord($1)) /eg;
+    print indent("Chunk: '$disp4cc' ($csize bytes)\n", $indent);
+    if ($cfourcc eq 'VP8 ') {
+      if ($csize >= 10) {
+	my ($ft, $mag, $width, $height) = unpack("a3a3S<S<", $cbody);
+	$ft = "$ft\0";
+	$ft = unpack("L<", $ft);
+	my $frame_type = ($ft >> 23) & 1 ? "inter" : "key";
+	my $version = ($ft >> 20) & 7;
+	my $show_frame = ($ft >> 19) & 1;
+	$ft &= 0x7FFFF;
+	my $mag_status = ($mag eq "\x9d\x01\x2a") ? "Good" : "Bad";
+	my $disp_mag = unpack "H*", $mag;
+	my $hscale = ($width >> 14) & 3;
+	my $vscale = ($height >> 14) & 3;
+	$width &= 0x3fff;
+	$height &= 0x3fff;
+      print indent(<<EOS, $indent);
   Type: $frame_type
   Version: $version
   Show Frame: $show_frame
@@ -55,25 +60,25 @@ while (read($fh, $chead, 8) == 8) {
   Scale: $hscale x $vscale
   First size: $ft
 EOS
+      }
+      else {
+	print indent(" ** body too small for 'VP8 '\n", $indent);
+      }
     }
-    else {
-      print " ** body too small for 'VP8 '\n";
-    }
-  }
-  elsif ($cfourcc eq 'VP8X') {
-    if ($csize >= 10) {
-      my ($flags, $res, $cwidth, $cheight) = unpack("Ca3a3a3", $cbody);
-      my $res_a = ($flags & 0xC0);
-      my $has_icc = ($flags & 0x20) ? "Yes" : "No";
-      my $has_alpha = ($flags & 0x10) ? "Yes" : "No";
-      my $has_exif = ($flags & 0x08) ? "Yes" : "No";
-      my $has_xmp = ($flags & 0x04) ? "Yes" : "No";
-      my $has_anim = ($flags & 0x02) ? "Yes" : "No";
-      my $res_b = ($flags & 0x01);
-      my $res_c = unpack("H*", $res);
-      $cwidth = unpack("L<", "$cwidth\0");
-      $cheight = unpack("L<", "$cheight\0");
-      print <<EOS;
+    elsif ($cfourcc eq 'VP8X') {
+      if ($csize >= 10) {
+	my ($flags, $res, $cwidth, $cheight) = unpack("Ca3a3a3", $cbody);
+	my $res_a = ($flags & 0xC0);
+	my $has_icc = ($flags & 0x20) ? "Yes" : "No";
+	my $has_alpha = ($flags & 0x10) ? "Yes" : "No";
+	my $has_exif = ($flags & 0x08) ? "Yes" : "No";
+	my $has_xmp = ($flags & 0x04) ? "Yes" : "No";
+	my $has_anim = ($flags & 0x02) ? "Yes" : "No";
+	my $res_b = ($flags & 0x01);
+	my $res_c = unpack("H*", $res);
+	$cwidth = unpack("L<", "$cwidth\0");
+	$cheight = unpack("L<", "$cheight\0");
+	print indent(<<EOS, $indent);
   Has ICC: $has_icc
   Has Alpha: $has_alpha
   Has EXIF: $has_exif
@@ -83,24 +88,79 @@ EOS
   Canvas Height: $cheight + 1
   Reserved: $res_a  $res_b  $res_c
 EOS
+      }
+      else {
+	print " ** body too small for 'VP8X'\n";
+      }
     }
-    else {
-      print " ** body too small for 'VP8X'\n";
-    }
-  }
-  elsif ($cfourcc eq 'ALPH') {
-    if ($csize > 1) {
-      my ($flags) = unpack("C", $cbody);
-      my $pre = ($flags & 0x30) >> 4;
-      my $filt = ($flags & 0xC0) >> 2;
-      my $comp = ($flags & 0x03);
-      my $leftover = $csize - 1;
-      print <<EOS;
+    elsif ($cfourcc eq 'ALPH') {
+      if ($csize > 1) {
+	my ($flags) = unpack("C", $cbody);
+	my $pre = ($flags & 0x30) >> 4;
+	my $filt = ($flags & 0xC0) >> 2;
+	my $comp = ($flags & 0x03);
+	my $leftover = $csize - 1;
+	print indent(<<EOS, $indent);
   Preprocessing: @{["None", "Level Reduction", "Unknown", "Unknown" ]}[$pre] ($pre)
   Filtering: @{["predictor = 0", "predictor = A", "predictor = B", "predictor = clip(A+B-C)"]}[$filt] ($filt)
   Compression: @{["None", "WebP Lossless", "Unknown", "Unknown" ]}[$comp] ($comp)
   Alpha bytes: $leftover
 EOS
+      }
+      else {
+	print indent(" ** body too small for 'ALPH'\n", $indent);
+      }
+    }
+    elsif ($cfourcc eq 'ANIM') {
+      if ($csize >= 6) {
+	my ($bgc, $loop) = unpack("a4S<", $cbody);
+	$bgc = unpack("H*", $bgc);
+	print indent(<<EOS, $indent)
+  Background: $bgc
+  Loops: $loop
+EOS
+      }
+      else {
+	print indent(" ** body too small for 'ANIM'\n", $indent);
+      }
+    }
+    elsif ($cfourcc eq 'ANMF') {
+      if ($csize >= 16) {
+	my ($framex, $framey, $fwidth, $fheight, $dur, $flags) =
+	  unpack("a3a3a3a3a3C", $cbody);
+	$framex = unpack("L<", "$framex\0");
+	$framey = unpack("L<", "$framey\0");
+	$fwidth = unpack("L<", "$fwidth\0");
+	$fheight = unpack("L<", "$fheight\0");
+	$dur = unpack("L<", "$dur\0");
+	my $blend = ($flags & 0x02) ? "Do no blend" : "Blend alpha";
+	my $dispose = ($flags & 0x01) ? "Yes" : "No";
+	my $framelen = $csize - 16;
+	print <<EOS;
+  Frame X: $framex
+  Frame Y: $framey
+  Width: $fwidth + 1
+  Height: $fheight + 1
+  Duration: $dur ms
+  Blend: $blend
+  Dispose: $dispose
+  Frame data: $framelen bytes
+EOS
+	my $data = substr($cbody, 16);
+	open my $subfh, "<", \$data;
+	do_dump($subfh, $indent+2);
+      }
+      else {
+	print indent(" ** body too small for 'ANMF'\n", $indent);
+      }
     }
   }
+}
+
+sub indent {
+  my ($text, $level) = @_;
+
+  my $indent = "  " x $level;
+  $text =~ s/^/$indent/gm;
+  $text;
 }
